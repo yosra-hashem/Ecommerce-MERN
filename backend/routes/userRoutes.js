50b0b756a5dfe68be377294d3e74ab1a1765ae3a
mohy-dev <mohy.dev@gmail.com>
2022-07-05 13:13:38 +0200

    routes

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const router = express.Router();
+const {
+  authUser,
+  registerUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+  getUserByID,
+  updateUser,
+  forgotPassword,
+  resetPassword,
+} = require("../controllers/userController.js");
+const { admin, protect } = require("../middleware/authMiddleware.js");
+const requestsLimiter = require("../utils/requestsLimiter.js");
+
+router
+  .route("/")
+  .post(requestsLimiter, registerUser)
+  .get(protect, admin, getUsers);
+
+router.post("/login", requestsLimiter, authUser);
+
+router.route("/forgotpassword").post(protect, forgotPassword);
+
+router.route("/passwordreset/:resetToken").put(protect, resetPassword);
+
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
+
+router
+  .route("/:id")
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserByID)
+  .put(protect, admin, updateUser);
+
+module.exports = router;
