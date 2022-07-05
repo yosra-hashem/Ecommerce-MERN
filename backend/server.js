24ebeeaa252afdff5e2cdd38f00cb04c5d774641
laila672 <lailatariq672@gmail.com>
2022-07-05 21:12:29 +0200

    add server file

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,54 @@
+const path = require("path");
 const express = require("express");
-const app = express();
-const port = 3000;
+const morgan = require("morgan");
+const cors = require("cors");
+const connectDB = require("./config/db.js");
+const userRoutes = require("./routes/userRoutes.js");
+const orderRoutes = require("./routes/orderRoutes.js");
+const productRoutes = require("./routes/productRoutes.js");
 
-app.get("/", (req, res) => res.send("Hello World!"));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const { notFound, errorHandler } = require("./middleware/errorMiddleware.js");
+
+require("dotenv").config();
+
+connectDB();
+
+const app = express();
+
+if (process.env.NODE_ENV === "developement") {
+  app.use(morgan("dev"));
+}
+
+app.use(express.json(), express.urlencoded({ extended: false }));
+app.use(cors());
+
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
+app.get("/api/config/paypal", (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+);
+
+var __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("Server is running");
+  });
+}
+
+// app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
